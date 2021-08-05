016fc019ce12503a08c53008d0e3918e5013e8e5
ark55555 <ark.tooyoung@gmail.com>
2021-08-05 12:12:31 +0000

    [Add]routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,42 @@
 Rails.application.routes.draw do
-
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :customers, controllers: {
-    sessions: 'public/customers/sessions',
-    registrations: 'public/customers/registrations'
+    sessions: 'public/sessions',
+    registrations: 'public/registrations'
   }
+
 
   devise_for :admins, controllers: {
-    sessions: 'admin/admins/sessions',
-    registrations: 'admin/admins/registrations'
+    sessions: 'admin/sessions'
   }
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
+
   namespace :admin do
-    resources :items
-    resources :orders
-    resources :orders_items
-    resources :genres
-    
+    # adminログイン後遷移ページ
+    root :to => "orders#index"
+    resources :customers, only: [:index, :show, :edit, :update]
+    resources :items, except: [:destroy]
+    resources :genres, only: [:index, :create, :edit, :update]
+    resources :orders, only: [:show, :update] do
+      resources :orders_items, only: [:update]
+    end
+    # get "search" => "searches#search"
   end
+
+  scope module: :public do
+    root "items#top"
+    get "/about" => "items#about"
+    get "customers/my_page" => "customers#show"
+    get "customers/unsubscribe" => "customers#unsubscribe"
+    get "orders/complete"=> "orders#complete"
+    post "orders/confirm" => "orders#confirm"
+    patch "customers/withdraw" => "customers#withdraw"
+    delete "cart_items/destroy_all" => "cart_items#destroy_all"
+    resource :customers, only: [ :edit, :update]
+    resources :items, only: [:index, :show]
+    resources :cart_items, only: [:index, :create, :update, :destroy]
+    resources :orders, only: [:new, :create, :index, :show]
+    resources :deliveries, only: [:index, :create, :edit, :update, :destroy]
+    # get "search" => "searches#search"
+  end
+
 end
