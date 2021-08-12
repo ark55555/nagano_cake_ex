d7817e2e51ccdf13494644dcf9f4b4d49e6bdf34
ark55555 <ark.tooyoung@gmail.com>
2021-08-12 09:20:12 +0000

    [Add]item-show/amount select

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -6,11 +6,13 @@ class Public::ItemsController < ApplicationController
   def index
     @items = Item.all
   end
-  
+
   def about
   end
-  
+
   def show
+    @item = Item.find(params[:id])
+    @cart_item = CartItem.new
   end
-  
+
 end
