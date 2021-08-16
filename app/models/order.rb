11096409b6b841906d859202b03658a278230dba
ark55555 <ark.tooyoung@gmail.com>
2021-08-16 07:06:41 +0000

    [Add]model-order/order_item

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,15 @@
 class Order < ApplicationRecord
+
+  belongs_to :customer
+  has_many :order_items, dependent: :destroy
+
+ validates :total_price, presence: true, numericality: {greater_than_or_equal_to: 0 }
+ validates :shipping_cost, presence: true, numericality: {greater_than_or_equal_to: 0 }
+ validates :delivery_address, presence: true
+ validates :delivery_postcode, presence: true, format: { with: /\A\d{7}\z/ }
+ validates :delivery_name, presence: true
+ 
+ enum payment_method: { クレジットカード: 0, 銀行振込: 1 }
+ enum status: { 入金待ち: 0, 入金確認: 1, 制作中: 2, 発送準備中: 3, 発送済: 4 }
+ 
 end
--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,2 +1,14 @@
 class OrderItem < ApplicationRecord
+  
+  belongs_to :item
+  belongs_to :order
+  
+  validates :price, presence: true, numericality: {greater_than_or_equal_to: 0 }
+  validates :amount, presence: true, numericality: {greater_than_or_equal_to: 1 }
+  
+  enum making_status: { 着手不可: 0, 製作待ち: 1, 制作中: 2, 製作完了: 3 }
+  
+  def subtotal
+    price * amount
+  end
 end
