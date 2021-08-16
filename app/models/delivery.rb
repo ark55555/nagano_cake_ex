b6323466541aa1ecde4600eb90f00dad24280cef
ark55555 <ark.tooyoung@gmail.com>
2021-08-16 08:13:18 +0000

    [Add]model-delivery

--- app/models/delivery.rb
+++ app/models/delivery.rb
@@ -1,2 +1,13 @@
 class Delivery < ApplicationRecord
+  
+  belongs_to :customer
+  
+  validates :postcode, presence: true, format: { with: /\A\d{7}\z/ }
+  validates :destination, presence: true
+  validates :name, presence: true
+  
+  def full_address
+    '〒' + postcode + ' ' + destination + ' ' + name
+  end
+  
 end
