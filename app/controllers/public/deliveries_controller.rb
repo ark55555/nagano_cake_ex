5c847456ee110b0b9b893ab36c13455da95a6b67
ark55555 <ark.tooyoung@gmail.com>
2021-08-19 09:12:41 +0000

    [Add]delivery-index

--- app/controllers/public/deliveries_controller.rb
+++ app/controllers/public/deliveries_controller.rb
@@ -1,9 +1,19 @@
 class Public::DeliveriesController < ApplicationController
   
   def index
+    @delivery = Delivery.new
+    @deliveries = current_customer.deliveries
   end
   
   def create
+    @delivery = Delivery.new(delivery_params)
+    @delivery.customer_id = current_customer.id
+    if @delivery.save
+      redirect_to deliveries_path, notice: '配送先を登録しました'
+    else
+      @deliveries = current_customer.deliveries
+      render :index
+    end
   end
   
   def edit
@@ -13,6 +23,12 @@ class Public::DeliveriesController < ApplicationController
   end
   
   def destroy
+  end
+  
+  private
+  
+  def delivery_params
+    params.require(:delivery).permit(:name, :destination, :postcode)
   end
   
 end
