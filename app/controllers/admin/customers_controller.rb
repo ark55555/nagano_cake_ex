211227628cc2bb1e6428239da1e5ce2331f35564
ark55555 <ark.tooyoung@gmail.com>
2021-09-09 08:59:14 +0000

    [Add]admin-customer/edit

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,16 +1,32 @@
 class Admin::CustomersController < ApplicationController
+  before_action :set_customer, only: [:show, :edit, :update]
 
   def index
     @customers = Customer.all
   end
 
   def show
-    @customer = Customer.find(params[:id])
   end
 
   def edit
   end
 
   def update
+    if @customer.update(customer_params)
+      redirect_to admin_customer_path(@customer), flash: {success: "会員情報を更新しました"}
+    else
+      render :edit
+    end
   end
+  
+  private
+  
+  def customer_params
+    params.require(:customer).permit(:email, :last_name, :first_name, :last_name_kana, :first_name_kana, :postcode, :address, :phone_number, :is_active)
+  end
+  
+  def set_customer
+    @customer = Customer.find(params[:id])
+  end
+  
 end
