43f8610740fba29268bfb1dfcdf36a7ba57c617d
ark55555 <ark.tooyoung@gmail.com>
2021-09-10 08:56:38 +0000

    [Fix]admin-order_item/making_status

--- app/models/order.rb
+++ app/models/order.rb
@@ -10,7 +10,7 @@ class Order < ApplicationRecord
  validates :delivery_name, presence: true
 
  enum payment_method: { クレジットカード: 0, 銀行振込: 1 }
- enum status: { 入金待ち: 0, 入金確認: 1, 制作中: 2, 発送準備中: 3, 発送済: 4 }
+ enum status: { 入金待ち: 0, 入金確認: 1, 製作中: 2, 発送準備中: 3, 発送済: 4 }
 
  def change_making_status
    if self.status == "入金待ち"
--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -6,16 +6,17 @@ class OrderItem < ApplicationRecord
   validates :price, presence: true, numericality: {greater_than_or_equal_to: 0 }
   validates :amount, presence: true, numericality: {greater_than_or_equal_to: 1 }
 
-  enum making_status: { 着手不可: 0, 製作待ち: 1, 制作中: 2, 製作完了: 3 }
+  enum making_status: { 着手不可: 0, 製作待ち: 1, 製作中: 2, 製作完了: 3 }
 
   def subtotal
     price * amount
   end
 
  def change_order_status
-   if self.making_status == "制作中"
+   order_items = self.order.order_items
+   if self.making_status == "製作中"
      self.order.update(status: 2)
-   elsif self.making_status == "製作完了"
+   elsif order_items.pluck(:making_status).all?{ |making_status| making_status == "製作完了"}
      self.order.update(status: 3)
    end
  end
