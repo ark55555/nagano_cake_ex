18b2d7c026347b853cce26c542c175f3d11ec604
ark55555 <ark.tooyoung@gmail.com>
2021-09-13 06:57:25 +0000

    [Add]genre-search

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -6,7 +6,13 @@ class Public::ItemsController < ApplicationController
   end
 
   def index
-    @items = Item.all
+    @genres = Genre.all
+    if params[:genre_id]
+      @genre = @genres.find(params[:genre_id])
+      @items = @genre.items.all
+    else
+      @items = Item.all
+    end
   end
 
   def about
